745148435d000f3ecd734b88a3a765eac0032bf9
CGSantiagoP <cgsantiagop@gmail.com>
2023-04-23 17:37:42 -0700

    I have added the LoginActivity adn SignUpActivity java files along with their xml files. They have (as far as I can currently tell) full functionality.
    To be done: Must connect MainActivity to be able to pick a button and lead to either option. Also, we must decide which values will remain. I put the common values (used username and no email for now) but we can add or remove values.
    
    Pressing commit and push because I'm not sure if things updated

---
+++ app/src/main/java/com/example/assignmenttracker/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.assignmenttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView txtSignUp;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtSignUp = findViewById(R.id.txtSignUp);
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
