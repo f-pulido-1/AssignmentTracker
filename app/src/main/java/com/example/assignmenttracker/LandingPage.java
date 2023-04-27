19e3e1da443edcff88c1b7da1a8a88d6f5cf269e
CGSantiagoP <cgsantiagop@gmail.com>
2023-04-27 01:08:49 -0700

    Added a button to log out

--- app/src/main/java/com/example/assignmenttracker/LandingPage.java
+++ app/src/main/java/com/example/assignmenttracker/LandingPage.java
@@ -1,5 +1,40 @@
 package com.example.assignmenttracker;
 
-public class LandingPage {
-//    Comment here
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LandingPage extends AppCompatActivity {
+
+    private Button backButton;
+    private Button landingAddAssignmentBtn;
+    private Button landingEnterGradeBtn;
+    private TextView landingMessage;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+        backButton = findViewById(R.id.backButton);
+        landingAddAssignmentBtn = findViewById(R.id.landingAddAssignmentBtn);
+        landingEnterGradeBtn = findViewById(R.id.landingEnterGradeBtn);
+        landingMessage = findViewById(R.id.landingMessage);
+        imageView = findViewById(R.id.imageView);
+
+        String username = getIntent().getStringExtra("username");
+        landingMessage.setText("Welcome, " + username + "!");
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
 }
